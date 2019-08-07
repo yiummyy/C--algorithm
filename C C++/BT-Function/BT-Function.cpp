56adef410e040330889817dfea72d2c3fa6f0b2f
Myth.Li <42298789+yiummyy@users.noreply.github.com>
2019-08-08 06:11:42 +0800

    Update BT-Function.cpp

--- C C++/BT-Function/BT-Function.cpp
+++ C C++/BT-Function/BT-Function.cpp
@@ -339,6 +339,55 @@ tNode* deleteNode(tNode* x, int key)
 	return x;
 }
 
+struct stack
+{
+	tNode **array;
+	int index;
+	
+	stack()
+	{
+		array = new tNode*[100];
+		index = 0;//index pointing to next position which you are going to wright
+	}
+	~stack()
+	{
+		delete[] array;
+	}
+	void push(tNode* node)
+	{
+		if(index < 100)
+		{
+			*(array + index) = node;
+			index++;
+		}
+	}
+	tNode* pos()
+	{
+		if(index > 0)
+		{
+			index--;
+			return *(array+index);
+		}
+		else
+		{
+			return NULL;
+		}
+	}
+	int getLength()
+	{
+		return index;
+	}
+	tNode* get(int i)
+	{
+		tNode* p = NULL;
+		if(i > 0 && i < index)//0~index-1
+		{
+			p = *(array + i);
+		}
+		return p;
+	}
+}
+}
 int main(int argc, char** argv) 
 {
 	int array[] = {19,22,31,24,55,3,2,9,17,25,53};
